e58e8de3c61a02164bd94aae6436825416317276
Mikhail Kryloff <edaurdgriffin@gmail.com>
2022-07-13 19:06:21 +0500

    Cursor non locked

--- Assets/ExportPackage/Scripts/Systems/ObserveSystem/PlayerObserveSystem.cs
+++ Assets/ExportPackage/Scripts/Systems/ObserveSystem/PlayerObserveSystem.cs
@@ -3,16 +3,11 @@ using UnityEngine;
 
 namespace GameLogic
 {
-    public class PlayerObserveSystem : IEcsInitSystem, IEcsRunSystem
+    public class PlayerObserveSystem : IEcsRunSystem
     {
         private readonly EcsFilter<PlayerObserveComponent> cameras = null;
         private readonly EcsFilter<InputComponent> input = null;
-        
-        public void Init()
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-        }
-
+  
         public void Run()
         {
             foreach (var inputFilter in input)
